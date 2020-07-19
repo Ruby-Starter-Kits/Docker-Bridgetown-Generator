10c3ac70951cafd276afb81f30b0e2c19292c2c4
Mike Rogers <me@mikerogers.io>
2020-07-19 16:33:38 +0100

    Using rsync to copy hidden files from App-Template (#13)

--- docker/interactive-install.rb
+++ docker/interactive-install.rb
@@ -7,17 +7,11 @@ require "readline"
   '--skip-yarn'
 ].join(' ')
 
-# TODO:
-# Look into adding the:
-# --apply="https://github.com/ParamagicDev/bridgetown-automation-docker-compose"
-# argument in. It could reduce the duplication of approaches to docker for Bridgetown.
-
 # Install bridgetown
 system("bridgetown new #{@app_path} #{@arguments} #{ENV['BRIDGETOWN_INSTALL_ARGS']}")
 
 # Copy the docker files
-system("shopt -s dotglob")
-system("cp -ra /usr/src/App-Template/* /usr/src/app/#{@app_path}/")
+system("rsync -a /usr/src/App-Template/ /usr/src/app/#{@app_path}/")
 
 # Copy sample files
 system("cp -ra /usr/src/App-Template/.env.sample /usr/src/app/#{@app_path}/.env")
